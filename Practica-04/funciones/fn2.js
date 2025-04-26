0cae3fa8518f8931b7cade82b03618d3546630c1
vfigunn <vfigun@gmail.com>
2025-04-26 17:26:16 -0300

    PokeBattle

--- Practica-04/funciones/fn2.js
+++ Practica-04/funciones/fn2.js
@@ -30,16 +30,28 @@ const Batalla = async (id1,id2)=>{
 
         let p1 = document.querySelector('#p1')
         p1.textContent = nombrePoke1.toUpperCase()
+        let img1 = document.querySelector('#img1')
+        img1.src = pokemon1.sprites.front_default
+        let poder1 = document.querySelector('#poder1')
+        poder1.textContent = `Ataque: ${poderPoke1.toString()}`
+
 
         let p2 = document.querySelector('#p2')
         p2.textContent = nombrePoke2.toUpperCase()
+        let img2 = document.querySelector('#img2')
+        img2.src = pokemon2.sprites.front_default
+        let poder2 = document.querySelector('#poder2')
+        poder2.textContent = `Ataque: ${poderPoke2.toString()}`
 
         let ganador = document.querySelector('#ganador')
+        let imgganador = document.querySelector("#imgganador")
 
         if (poderPoke1>poderPoke2){
             ganador.textContent = `${nombrePoke1.toUpperCase()} es el ganador!!`
+            imgganador.src = img1.src            
         }else if(poderPoke1<poderPoke2){
-            ganador.textContent = `${nombrePoke2.toUpperCase()} es el ganador!!`
+            ganador.innerHTML = `<b>${nombrePoke2.toUpperCase()}</b> es el ganador!!`
+            imgganador.src = img2.src
         }else{
             ganador.textContent = `Tienen el mismo poder de ataque!!`
         }
@@ -51,8 +63,6 @@ const Batalla = async (id1,id2)=>{
 }
 
 
-let id1 = parseInt(document.querySelector('#poke1').value)
-let id2 = parseInt(document.querySelector('#poke1').value)
 
 let btnBatalla = document.querySelector('#batalla')
 
@@ -63,4 +73,26 @@ btnBatalla.addEventListener('click',(e)=>{
     let id2 = parseInt(document.querySelector('#poke2').value)
 
     Batalla(id1,id2)
+})
+
+let btnBorrar = document.querySelector('#borrar')
+
+btnBorrar.addEventListener('click',(e)=>{
+    e.preventDefault()
+
+    let id1 = document.querySelector("#poke1").value = ""
+    let id2 = document.querySelector("#poke2").value = ""
+
+    let p1 = document.querySelector('#p1').textContent = ""
+    let img1 = document.querySelector('#img1').src = ""
+
+    let p2 = document.querySelector('#p2').textContent = ""
+    let img2= document.querySelector('#img2').src = ""
+
+    let ganador = document.querySelector('#ganador').textContent = ""
+    let imgganador = document.querySelector("#imgganador").src = ""
+
+    let poder1 = document.querySelector('#poder1').textContent = ""
+    let poder2 = document.querySelector("#poder2").textContent = ""
+
 })
